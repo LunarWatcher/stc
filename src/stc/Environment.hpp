772ce6fe59eca7f7c0027c5a96a342fafdaaa671
Olivia <zoe.i2k1@gmail.com>
2021-09-15 22:01:05 +0200

    Add syscommand (initial dependent: upm)

--- src/stc/Environment.hpp
+++ src/stc/Environment.hpp
@@ -246,4 +246,16 @@ inline std::string getHome() {
     return homePath;
 }
 
+inline std::string syscommand(const std::string& command) {
+    std::array<char, 128> buffer;
+    std::string res;
+
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command.c_str(), "r"), pclose);
+    if (!pipe) throw std::runtime_error("Failed to run " + command);
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        res += buffer.data();
+    }
+    return res;
+}
+
 }
