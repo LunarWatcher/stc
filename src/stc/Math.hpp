a87dee406b61e6a56a0681c52ba0c0e3939c6138
Olivia <oliviawolfie@pm.me>
2025-12-09 20:56:59 +0100

    Optimisation: prevent the inclusive variant from effectively overriding the opposite check in the exclusive variant

--- src/stc/Math.hpp
+++ src/stc/Math.hpp
@@ -129,14 +129,8 @@ inline bool lineIntersectsLineInclusive(
     const VT& l2Start,
     const VT& l2End
 ) {
-    if (l1Start == l2Start && l1End == l2End) {
-        return true;
-    }
-    return lineIntersectsLineExclusive<IT, VT>(
-        l1Start,
-        l1End,
-        l2Start,
-        l2End
+    return (isCounterClockwise<IT, VT>(l1Start, l2Start, l2End) != isCounterClockwise<IT, VT>(l1End, l2Start, l2End)
+        && isCounterClockwise<IT, VT>(l1Start, l1End, l2Start) != isCounterClockwise<IT, VT>(l1Start, l1End, l2End)
     )
         || isPointOnLeftOfEdge<IT, VT>(l1Start, l2Start, l2End) == 0
         || isPointOnLeftOfEdge<IT, VT>(l1End, l2Start, l2End) == 0;
