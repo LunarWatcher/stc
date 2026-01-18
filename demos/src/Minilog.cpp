3492dbc4fd21539cde34907af003e187b23954d1
Olivia <oliviawolfie@pm.me>
2026-01-18 07:56:44 +0100

    Include config in demo

--- demos/src/Minilog.cpp
+++ demos/src/Minilog.cpp
@@ -1,6 +1,7 @@
 #include <stc/minilog.hpp>
 
 int main() {
+    // minilog::config().level = minilog::Level::ERROR;
     minilog::debug("This is info");
     minilog::info("This is info");
     minilog::warn("This is info");
