7f2c512ba20e016bd373427ed1b1f0658448b727
Olivia <oliviawolfie@pm.me>
2026-01-24 17:38:45 +0100

    Forgot finishing a comment

--- tests/src/bench/MinilogBench.cpp
+++ tests/src/bench/MinilogBench.cpp
@@ -4,6 +4,8 @@
 #include <catch2/benchmark/catch_benchmark.hpp>
 
 TEST_CASE("Minilog benchmark", "[benchmark]") {
+    // This is technically not entirely representative, because we now store the output in memory rather than actually
+    // output, but it's fine
     stc::testutil::CaptureStandardStreams c;
     // The std::format benchmarks are used to get a measure of how much overhead there is in the logger.
     // If std::format does 100it/s and the logger does 99it/s, the actual performance of the actual logging bit is
